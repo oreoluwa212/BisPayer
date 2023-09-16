6e493544c0feb372630f0bcf325f7ecf418a9435
oreoluwa212 <105554286+oreoluwa212@users.noreply.github.com>
2023-09-17 00:19:31 +0100

    Reviews Page

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -13,7 +13,51 @@ const Reviews = () => {
         </div>
       </section>
 
-      <section className="reviews-container"></section>
+      <section className="reviews-container">
+        <div className="reviews-section">
+          <div className="text">
+            <h1>Our Clients Speak</h1>
+            <p>We have been working with clients around the world</p>
+          </div>
+          <div className="packages-grid">
+            <div className="grid1">
+              {/* <img src={Frame1} width="300" alt="" /> */}
+              <div className="h3">
+                <h3>Efficient Collaborating</h3>
+                <h4>
+                  Your team's approach to collaboration is outstanding. They
+                  actively listen to our ideas and concerns, fostering a
+                  productive partnership
+                </h4>
+                <p>-Lauren Jude</p>
+              </div>
+            </div>
+            <div className="grid1">
+              {/* <img src={Frame2} width="300" alt="" /> */}
+              <div className="h3">
+                <h3>Intuitive Design</h3>
+                <h4>
+                  The intuitive design of your products and services is a
+                  game-changer. Everything feels user-friendly and effortless .
+                </h4>
+                <p>-Asa Daniels</p>
+              </div>
+            </div>
+            <div className="grid1">
+              {/* <img src={Frame3} width="300" alt="" /> */}
+              <div className="h3">
+                <h3>Mind-Blowing Service</h3>
+                <h4>
+                  Your service consistently goes above and beyond our
+                  expectations. The level of dedication and attention to detail
+                  is truly mind-blowing.{" "}
+                </h4>
+                <p>-Johnny Drille</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
     </main>
   );
 };
